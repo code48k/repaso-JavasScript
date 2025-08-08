036cc555ac17379ac67bb31b010c242e5847d92e
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-08 21:03:22 +0200

    Operadores

--- operadores.js
+++ operadores.js
@@ -4,7 +4,7 @@
 
 // NOS VAMOS A CONCENTRAR EN LOS ASPECTOS DE LOS OPERADORES QUE NO ESTÁN CUBIERTOS EN LA ARITMÉTICA ESCOLAR.
 
-// UN OPERADOR ES UN SÍMBOLO O PALABRA CLAVE QUE LE DICE A JAVASCRIPT QUE HAGA UNA OPERACIÓN ENTRE UNO O MÁS VALORES (LOS OPERANDOS).
+// UN OPERADOR ES UN SÍMBOLO O PALABRA CLAVE QUE LE DICE A JAVASCRIPT, QUE HAGA UNA OPERACIÓN ENTRE UNO O MÁS VALORES (LOS OPERANDOS).
 
 // EN OTRAS PALABRAS, ES LO QUE USÁS PARA SUMAR, RESTAR, COMPARAR, ASIGNAR, ETC...
 
@@ -47,7 +47,7 @@ console.log(y2 - x2); // 2, binario negativo resta valores
 
 // EXPONENCIACIÓN
 
-// LOS PRIMEROS CUATRO SON CONOCIDOS MIENTRAS QUE % Y ** DEBEN SER EXPLICADOS MÁS AMPLIAMENTE.
+// LOS PRIMEROS CUATRO SON CONOCIDOS MIENTRAS QUE '%' Y '**' DEBEN SER EXPLICADOS MÁS AMPLIAMENTE.
 
 // ------------------------------------------------------------------------------------------------------------------------
 // SUMA +
@@ -76,7 +76,7 @@ console.log(8 * 4); // 32, el resultado de la multiplicación
 
 // DIVISIÓN /
 
-// EL OPERADOR DE LA DIVISIÓN '*', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO DE UNA DIVISIÓN, POR LO CUAL OBTENEMOS EL RESULTADO.
+// EL OPERADOR DE LA DIVISIÓN '/', SÍ SE APLICA A DOS OPERANDOS, LAS CONVIERTE EN RESULTADO DE UNA DIVISIÓN, POR LO CUAL OBTENEMOS EL RESULTADO.
 
 console.log(5 / 5); // 1, el resultado de la división
 console.log(8 / 2); // 4, el resultado de la división
@@ -84,7 +84,7 @@ console.log(8 / 4); // 2, el resultado de la división
 
 // RESTO %
 
-// EL OPERADOR RESTO %, A PESAR DE SU APARIENCIA, NO ESTÁ RELACIONADO CON PORCENTAJES.
+// EL OPERADOR RESTO '%', A PESAR DE SU APARIENCIA, NO ESTÁ RELACIONADO CON PORCENTAJES.
 
 // EL RESULTADO DE 'a % b' ES EL RESTO DE LA DIVISIÓN ENTERA DE 'a' POR 'b'.
 
@@ -110,9 +110,9 @@ console.log(2 ** 4); // 2⁴ = 16
 
 // POR EJEMPLO, LA RAÍZ CUADRADA ES EL EXPONENTE ½.
 
-console.log(4 ** (1/2)); // 2 (potencia de 1/2 es lo mismo que raíz 
+console.log(4 ** (1/2)); // 2 (potencia de 1/2 es lo mismo que raíz cuadrada) 
 console.log(8 ** (1/3)); // 2 (potencia de 1/3 es lo mismo que raíz cúbica)
-console.log(16 ** (1/4)); // 2 (potencia de 1/4 es lo mismo que raíz 
+console.log(16 ** (1/4)); // 2 (potencia de 1/4 es lo mismo que raíz cuarta)
 
 // ------------------------------------------------------------------------------------------------------------------------
 
@@ -300,8 +300,8 @@ d = f;
 // POR EJEMPLO:
 
 let n = 2;
-n = n + 5;
-n = n * 2;
+n = n + 5; // 7
+n = n * 2; // 14
 
 // ESTA NOTACIÓN PUEDE SER ACORTADA UTILIZANDO LOS OPERADORES '+=' Y '*='.
 
